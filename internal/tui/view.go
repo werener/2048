0b04b437bb7182650223dd14e13a2260007e7195
werener <lenyazhuk@yandex.ru>
2026-08-05 17:49:32 +0300

    refactor: use a built-in color manipulation for cleaner code

--- internal/tui/view.go
+++ internal/tui/view.go
@@ -94,53 +94,32 @@ func (m model) tileView(tile core.Tile, size int) string {
 func colorTile(tile core.Tile) (bg color.Color, fg color.Color, border color.Color, newlySpawnedBorder color.Color) {
 	switch tile.Value {
 	case 2:
-		fg = lipgloss.Color("#9f9fc7")
 		bg = lipgloss.Color("#2c2c79")
-		border = lipgloss.Color("#4545ad")
-		newlySpawnedBorder = lipgloss.Color("#5c5caa")
 	case 4:
-		fg = lipgloss.Color("#9d9dff")
-		bg = lipgloss.Color("#426292")
-		border = lipgloss.Color("#4f4fa1")
-		newlySpawnedBorder = lipgloss.Color("#7373d4")
+		bg = lipgloss.Color("#286994")
 	case 8:
-		fg = lipgloss.Color("#7fafe7")
 		bg = lipgloss.Color("#438594")
-		border = lipgloss.Color("#4496a8")
 	case 16:
-		fg = lipgloss.Color("#b4f8e7")
 		bg = lipgloss.Color("#328570")
-		border = lipgloss.Color("#3dad91")
 	case 32:
-		fg = lipgloss.Color("#aeffc0")
 		bg = lipgloss.Color("#236d39")
-		border = lipgloss.Color("#237e3d")
 	case 64:
-		fg = lipgloss.Color("#d8f5a3")
 		bg = lipgloss.Color("#7f9726")
-		border = lipgloss.Color("#8fac25")
 	case 128:
-		fg = lipgloss.Color("#ffd5a6")
 		bg = lipgloss.Color("#bd9100")
-		border = lipgloss.Color("#dda900")
 	case 256:
-		fg = lipgloss.Color("#ffc582")
 		bg = lipgloss.Color("#c25700")
-		border = lipgloss.Color("#be5601")
 	case 512:
-		fg = lipgloss.Color("#ff9182")
 		bg = lipgloss.Color("#8f0c0c")
-		border = lipgloss.Color("#a10c0c")
 	case 1024:
-		fg = lipgloss.Color("#ffadc6")
 		bg = lipgloss.Color("#770059")
-		border = lipgloss.Color("#91006c")
 	case 2048:
-		fg = lipgloss.Color("#f9ffc2")
 		bg = lipgloss.Color("#55007c")
-		border = lipgloss.Color("#6a009b")
-		// default:
-		// 	fg = lipgloss.Color
 	}
+
+	fg = lipgloss.Lighten(bg, 0.5)
+	border = lipgloss.Darken(bg, 0.15)
+	newlySpawnedBorder = lipgloss.Lighten(border, 0.3)
+	newlySpawnedBorder = lipgloss.Lighten(border, 0.3)
 	return
 }

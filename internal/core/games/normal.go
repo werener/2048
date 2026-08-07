7f4ae8320e3848780fa568b9bb7780e1ed0eeaa4
werener <lenyazhuk@yandex.ru>
2026-08-07 19:12:01 +0300

    feat: implement logic for Undo in a normal game

--- internal/core/games/normal.go
+++ internal/core/games/normal.go
@@ -8,6 +8,9 @@ import m "github.com/werener/2048.git/internal/core/models"
 type NormalGame struct {
 	grid  m.Grid
 	score m.Score
+
+	prevGrid  m.Grid
+	prevScore m.Score
 }
 
 // NewNormalGame initializes a [NormalGame]
@@ -48,22 +51,30 @@ func (game *NormalGame) State() m.GameState {
 	return m.DEFEAT
 }
 
+// Undo returns the game to its state as of last turn
+func (game *NormalGame) Undo() {
+	game.grid = game.prevGrid
+	game.score = game.prevScore
+}
+
 /*
 MakeMove performs a full move cycle in the following order:
 
  1. Checks if a move in this direction will change the state of the board.
-    If not - returns. Otherwise, executes steps 2-6.
- 2. Shifts all tiles in the provided direction;
- 3. Combines adjacent tiles with equal values;
- 4. Shifts all tiles in the provided direction;
- 5. Spawns a new random tile, using [Grid.SpawnTile] with [DefaultDistribution].
- 6. Increments [NormalGame.score] field by the amount, gained from this move.
+    If not - returns. Otherwise, executes steps 2-7.
+ 2. Saves previous grid and scores for further use in [NormalGame.Undo]
+ 3. Shifts all tiles in the provided direction;
+ 4. Combines adjacent tiles with equal values;
+ 5. Shifts all tiles in the provided direction;
+ 6. Spawns a new random tile, using [Grid.SpawnTile] with [DefaultDistribution].
+ 7. Increments [NormalGame.score] field by the amount, gained from this move.
 */
 func (game *NormalGame) MakeMove(direction m.Direction) {
 	if !game.grid.CanShift(direction) {
 		return
 	}
-	game.score += game.grid.Shift(direction)
+	game.prevScore, game.prevGrid = game.score, game.grid
 
+	game.score += game.grid.Shift(direction)
 	game.grid.SpawnTile(m.DefaultDistribution)
 }
